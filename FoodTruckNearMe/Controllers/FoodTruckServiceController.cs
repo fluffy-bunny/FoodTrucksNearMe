011806df7147146506d08b13606029d51bbfcab3
Herb Stahl <ghstahl@gmail.com>
2021-06-30 13:24:59 -0700

    missing controller

---
+++ FoodTruckNearMe/Controllers/FoodTruckServiceController.cs
@@ -0,0 +1,37 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodTruckNearMe.Controllers
+{
+   
+    [ApiController]
+    [Route("[controller]")]
+    public class FoodTruckServiceController : ControllerBase
+    {
+        private ISomething _something;
+        private IFoodTruckService _foodTruckService;
+        private ILogger<FoodTruckServiceController> _logger;
+
+        public FoodTruckServiceController(
+              ISomething something,
+              IFoodTruckService foodTruckService,
+              ILogger<FoodTruckServiceController> logger)
+        {
+            _something = something;
+            _foodTruckService = foodTruckService;
+            _logger = logger;
+        }
+        [HttpPost]
+        public async Task<ListFoodTruckPermitsResponse> GetPage(ListFoodTruckPermitsRequest request)
+        {
+            return await _foodTruckService.ListFoodTruckPermitsAsync(request); 
+        }
+    }
+}
+ 
